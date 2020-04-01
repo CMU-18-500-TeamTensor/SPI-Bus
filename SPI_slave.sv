913fff2000e73334a44a9fc946a277865c9006be
Jared Rodriguez <jarodrig@andrew.cmu.edu>
2020-04-01 12:44:55 -0400

    Correct protocol

--- SPI_slave.sv
+++ SPI_slave.sv
@@ -4,41 +4,33 @@ module SPI_slave_full (
   input  logic rst_L,
   input  logic sclk, ss, mosi,
   input  logic [7:0] outbuf,
-  output logic read,
-  output logic ready_in,
+  output logic sendrecv,
   output logic [7:0] buffer_in,
   output logic miso);
   
 logic [2:0] count;
 logic [7:0] buffer;
-logic start;
-assign miso = buffer[7];
+
+assign miso = ~ss ? buffer[7] : 1'bz;
   
 always_ff @(posedge sclk, negedge rst_L)
   if (~rst_L) begin
-    start <= 1'b0;
-    read <= 1'b0;
-    ready_in <= 1'b0;
+    sendrecv <= 1'b0;
+    buffer_in <= 1'b0;
     buffer <= 1'b0;
     count <= 1'b0;
   end else begin
-    read <= 1'b0;
-    ready_in <= 1'b0;
-    if (start) begin
+    sendrecv <= 1'b0;
+    if (~ss) begin
       count <= count + 1;
       buffer <= {buffer[6:0], mosi};
       if (count == 3'b111) begin
-        ready_in <= 1'b1;
+        sendrecv <= 1'b1;
         buffer_in <= {buffer[6:0], mosi};
-        start <= 1'b0;
+        buffer <= outbuf;
       end
     end else begin
       count <= 3'b000;
-    end
-    if (ss) begin
-      start <= 1'b1;
-      buffer <= outbuf;
-      read <= 1'b1;
     end
   end
   
--- SPI_slave_test.sv
+++ SPI_slave_test.sv
@@ -5,60 +5,52 @@ module testbench;
 logic sclk, ss, miso, mosi, rst_L;
 
 logic [7:0] outbuf;
-logic [7:0] recv;
-logic read;
+logic sendrecv;
 
-logic ready_in;
 logic [7:0] buffer_in;
 
 assign mosi = miso; // link to self
 
-task sendrecv ();
-    ss <= 1;
+task send_and_set_next ();
+  ss <= 0;
   for (int i = 0; i < 8; i++) begin
     @(posedge sclk);
-    ss <= 0;
   end
+  ss <= 1;
 endtask
 
 initial begin
   sclk = 0;
+  ss = 1;
   rst_L = 0;
   rst_L <= 1;
   forever #5 sclk = ~sclk;
 end
-
-initial begin
-  ss = 0;
-end
-  
 
 SPI_slave_full spi(.*);
 
 initial begin
   @(posedge sclk);
   @(posedge sclk);
-  outbuf = 8'hA5;
-  sendrecv();
   outbuf = 8'h5A;
-  sendrecv();
+  send_and_set_next();
+  send_and_set_next();
+  outbuf = 8'hA5;
+  send_and_set_next();
   outbuf = 8'hAA;
-  sendrecv();
+  send_and_set_next();
   outbuf = 8'hCC;
-  sendrecv();
+  @(posedge sclk)
+  send_and_set_next();
   outbuf = 8'h0F;
-  sendrecv();
+  send_and_set_next();
   outbuf = 8'hF0;
-  sendrecv();
+  send_and_set_next();
   @(posedge sclk);
   @(posedge sclk);
   @(posedge sclk) $finish;
 
 end
 
-
-
-always_ff @(posedge ready_in)
-  recv <= buffer_in;
 
 endmodule : testbench
